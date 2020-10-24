57946ef8c22121ea232f57e44c924740fea03aa1
EREN EROGLU <eren.eroglu@turkcell.com.tr>
2020-10-24 15:55:04 +0300

    refactoring: one more print function was added to trip_duration_stats method

--- bikeshare.py
+++ bikeshare.py
@@ -146,6 +146,8 @@ def station_stats(df):
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
+    
+    print("dummy print function")
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
